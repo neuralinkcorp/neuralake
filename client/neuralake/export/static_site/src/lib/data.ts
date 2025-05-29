d9dfa6b12601df0171cfd63f43b22b2a9746bf53
gautham acharya <gautham@neuralink.com>
2025-05-29 16:35:31 -0700

    [Web Catalog] Use precompiled vite build assets instead of invoking a NodeJS subprocess

--- client/neuralake/export/static_site/src/lib/data.ts
+++ client/neuralake/export/static_site/src/lib/data.ts
@@ -1,9 +1,8 @@
-// see https://vitejs.dev/guide/api-plugin.html#virtual-modules
-// see web_catalog/src/plugins/data-json-plugin.js
-
-// @ts-ignore
-import data from 'virtual:data-json'
 import { ExportedNeuralake } from './types'
 
-// We have to as-cast due to https://github.com/microsoft/TypeScript/issues/32063 and the type of invocation_type.
-export const neuralake = data as ExportedNeuralake
+const response = await fetch('/data.json')
+if (!response.ok) {
+  throw new Error(`Failed to load data: ${response.statusText}`)
+}
+
+export const neuralake = await response.json() as ExportedNeuralake
